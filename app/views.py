fe3c0f2bdd531c64c7e1d87faef8c6e7a04955f0
Reymark <reymarkvaldehueza@gmail.com>
2022-03-17 17:43:20 +0800

    update

--- app/views.py
+++ app/views.py
@@ -6,8 +6,9 @@ from django.contrib.sessions.models import Session
 from django.http import JsonResponse,HttpResponse
 from datetime import date
 import json
-from .models import card_tbl, gallery_photos
+from .models import card_tbl, gallery_photos, category1_tbl
 from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Q
 
 
 def login(request):
@@ -59,11 +60,40 @@ def updateStatus(request):
 def viewCard(request):
     if request.method=="POST":
         cardId = request.POST.get('id')
-        card = card_tbl.objects.all()
-    
-        card = {'data':"test"}
 
+
+
+        table_1 = card_tbl.objects.filter(id=cardId).values('category1_id')
+
+        table_2 = category1_tbl.objects.filter(Q(id=table_1))
+        
+
+        # table_2 = category1_tbl.objects.filter(Q(id='1'))
+
+
+        # ard_tbls = card_tbl.objects.get(id=cardId)
+        # cat1 = category1_tbl.objects.get(id=card_tbls)
+
+    
+
+
+        # cat1 = category1_tbl.objects.filter(name=Q(id='1'))
+
+        # card = card_tbl.objects.filter(category1_id=Q(id='Who'))
+        
+
+
+    
+        cardw = {'data':"test"}
+
+        # cat1 = {'data':cat1.name}
+
+
+        # cardss = {'data':cardss.category1_id}
+
+        # cat1 = {'data':cat1}
+        
         print("halaka")
-        print(cardId)
-        return JsonResponse(card)
+        print(table_2.name)
+        return JsonResponse(cardw)
 
